d99de969a4818a142194c5ca9865e0ba9d9ecddd
SantiagoTrifonArevalo <santiagodtrifon@gmail.com>
2023-09-28 04:23:38 +0200

    done

---
+++ datatypes.js
@@ -0,0 +1,52 @@
+//ejercicio 1
+//Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos//
+let age = 42;
+let time = 26.09;
+let hour = 20.45;
+let day = "Tuesday";
+const name = "Santy";
+const colorCar = "white";
+const nameFather = "Ricardo";
+const nameSong = "Gabriel";
+//ejercicio 2 
+//Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+console.log(typeof age);
+console.log(typeof time);
+console.log(typeof hour);
+console.log(typeof day);
+console.log(typeof name);
+console.log(typeof colorCar);
+console.log(typeof nameFather);
+console.log(typeof nameSong);
+//ejercicio 3
+//Declara cuatro variables sin asignar valores//
+let myAge;
+let temperature;
+let Children;
+let game;
+//ejercicio 4
+//Declara cuatro variables con valores asignados//
+const songName = "Gabriel";
+const modelPhone = "Xiomi";
+const sizeFoot = 45;
+const colorJean = "blue";
+//ejercicio 5
+//Declara variables para almacenar su nombre, apellido, estado civil, país y edad en múltiples líneas//
+const myName = "Santy";
+const surName = "Trifon";
+const civilStatus = "single";
+const country = "Spain";
+const age1 = 42;
+//ejercicio 6
+//Declara variables para almacenar su nombre, apellido, estado civil, país y edad en una sola línea//
+const myName1 = "Santy", surName1 = "Trifon", civilStatus1 = "single", country1 = "Spain", age2 = 42;
+//ejercicio 7
+//Declara dos variables miEdad y tuEdad y asígneles valores iniciales e imprimelas en la consola del navegador.//
+const myAge2 =42;
+const yourAge = 45;
+console.log(myAge2,yourAge);
+//ejercicio 8
+//Obten un numero por un prompt y hazlo visible con un alert.//
+let number = prompt ('elige un numeral, 5');
+alert (`has elegido el ${number}5`);
+console.log(number);
---
+++ variable.js
@@ -0,0 +1,12 @@
+//ejercicio 1
+//Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos//
+let age = 42;
+let time = 26.09;
+let hour = 20.45;
+let day = "Tuesday";
+const name = "Santy";
+const colorCar = "white";
+const nameFather = "Ricardo";
+const nameSong = "Gabriel";
+
+
